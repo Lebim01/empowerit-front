90127584458059ff24803d47e414cfeaf0e732cc
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2024-04-06 17:37:39 -0600

    welcome

--- src/views/auth/SignIn/SignIn.tsx
+++ src/views/auth/SignIn/SignIn.tsx
@@ -5,7 +5,8 @@ const SignIn = () => {
     <>
       <div className="mb-8">
         <div className="flex justify-center mb-8 md:hidden">
-          <img src="/img/welcome.png" className="w-full h-full object-cover" />
+          <img src="/img/welcome.png" className="w-full h-full object-cover hidden md:block" />
+          <img src="/img/welcome-mobile.png" className="w-full h-full object-cover md:hidden" />
         </div>
         <h3 className="mb-1">¡Bienvenido!</h3>
         <p>Por favor, escribe tu correo y contraseña para iniciar sesión</p>
