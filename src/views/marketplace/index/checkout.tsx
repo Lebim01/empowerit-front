6a6f8bffd5e6b66829dc0f30fb5f4a81d8981308
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-04-25 15:04:15 -0700

    palomita

--- src/views/marketplace/index/checkout.tsx
+++ src/views/marketplace/index/checkout.tsx
@@ -1,18 +1,20 @@
 import { db } from "@/configs/firebaseConfig"
 import { useAppSelector } from "@/store"
 import { currencyIcon } from "@/views/memberships/components/ButtonSwapCurrency"
-import { doc, getDoc, updateDoc } from "firebase/firestore"
+import { doc, getDoc, onSnapshot } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { FiCopy } from "react-icons/fi"
 import useClipboard from '@/utils/hooks/useClipboard'
 import { BsWallet } from "react-icons/bs"
 import { Input } from "@/components/ui"
+import { FaCheck } from "react-icons/fa"
 
 const MarketplaceCheckout = () => {
   const { copy } = useClipboard()
   const user = useAppSelector((state) => state.auth.user)
 
   const [cart, setCart] = useState<any[]>([])
+  const [paymentStatus, setPaymentStatus] = useState('')
   const [loading, setLoading] = useState(true)
   const [paymentLink, setPaymentLink] = useState({
     address: '',
@@ -22,8 +24,15 @@ const MarketplaceCheckout = () => {
   })
 
   useEffect(() => {
+    const unsub = onSnapshot(doc(db, `users/${user.uid}/cart/1`), (snap) => {
+      setPaymentStatus(snap.get('payment_link.status') || '')
+    })
     getLink()
     getCart()
+
+    return () => {
+      unsub()
+    }
   }, [])
 
   const getCart = async () => {
@@ -62,53 +71,71 @@ const MarketplaceCheckout = () => {
 
   return (
     <div>
-      <p className="font-bold underline underline-offset-8 mb-2 text-lg">Lista de compra</p>
+      <p className="font-bold underline underline-offset-8 mb-2 text-lg">
+        Lista de compra
+      </p>
       <div className="grid grid-cols-[min-content_1fr] gap-2 mb-8">
-        {cart.filter(p => p.quantity).map((p) => (
-          <>
-            <span className="font-bold">x{p.quantity}</span>
-            <span>{p.name}</span>
-          </>
-        ))}
+        {cart
+          .filter((p) => p.quantity)
+          .map((p) => (
+            <>
+              <span className="font-bold">x{p.quantity}</span>
+              <span>{p.name}</span>
+            </>
+          ))}
       </div>
-      <div className="flex flex-1 flex-col space-y-2 items-center max-w-[500px] w-full">
-        <img src={paymentLink.qr} />
 
-        <span>{user.email}</span>
+      {paymentStatus != 'confirming' && (
+        <div className="flex flex-1 flex-col space-y-2 items-center max-w-[500px] w-full">
+          <img src={paymentLink.qr} />
 
-        <Input
-          readOnly
-          prefix={<BsWallet />}
-          value={paymentLink.address}
-          suffix={
-            <div
-              className="bg-gray-200 p-2 rounded-lg hover:cursor-pointer hover:bg-gray-300"
-              onClick={() => copy(paymentLink.address)}
-            >
-              <FiCopy />
-            </div>
-          }
-        />
+          <span>{user.email}</span>
 
-        <div className={'grid gap-x-4 w-full'}>
           <Input
             readOnly
-            prefix={currencyIcon['LTC']}
-            value={paymentLink.amount.toFixed(8)}
+            prefix={<BsWallet />}
+            value={paymentLink.address}
             suffix={
-              <div className="flex items-center space-x-2">
-                <span>LTC</span>{' '}
-                <div
-                  className="bg-gray-200 p-2 rounded-lg hover:cursor-pointer hover:bg-gray-300"
-                  onClick={() => copy(paymentLink.amount.toFixed(8) || '')}
-                >
-                  <FiCopy />
-                </div>
+              <div
+                className="bg-gray-200 p-2 rounded-lg hover:cursor-pointer hover:bg-gray-300"
+                onClick={() => copy(paymentLink.address)}
+              >
+                <FiCopy />
               </div>
             }
           />
+
+          <div className={'grid gap-x-4 w-full'}>
+            <Input
+              readOnly
+              prefix={currencyIcon['LTC']}
+              value={paymentLink.amount.toFixed(8)}
+              suffix={
+                <div className="flex items-center space-x-2">
+                  <span>LTC</span>{' '}
+                  <div
+                    className="bg-gray-200 p-2 rounded-lg hover:cursor-pointer hover:bg-gray-300"
+                    onClick={() => copy(paymentLink.amount.toFixed(8) || '')}
+                  >
+                    <FiCopy />
+                  </div>
+                </div>
+              }
+            />
+          </div>
         </div>
-      </div>
+      )}
+
+      {paymentStatus == 'confirming' && (
+        <div className="flex flex-col justify-center items-center">
+          <span>{user?.email}</span>
+          <span>Se está confirmando tu transacción</span>
+          <span>Esto puede tardar varios minutos</span>
+          <div className="bg-green-400 rounded-full p-4 w-min mt-8">
+            <FaCheck fontSize={40} />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/views/marketplace/index/index.tsx
+++ src/views/marketplace/index/index.tsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import MarketplaceList from './list'
 import MarketplaceForm from './form'
 import MarketplaceCheckout from './checkout'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '@/configs/firebaseConfig'
+import { useAppSelector } from '@/store'
 
 const Marketplace = () => {
   const [stage, setStage] = useState<'cart' | 'form' | 'checkout'>('cart')
+  const user = useAppSelector((state) => state.auth.user)
+  const [cartPaymentStatus, setPaymentStatus] = useState<string>('')
+
+  useEffect(() => {
+    getCart()  
+  }, [])
+
+  const getCart = async () => {
+    const res = await getDoc(doc(db, `users/${user.uid}/cart/1`))
+    if(res.exists()){
+      try {
+        setPaymentStatus(res.get('payment_link.status'))
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+
+  useEffect(() => {
+    if(cartPaymentStatus == "confirming") {
+      setStage('checkout')
+    }
+  }, [cartPaymentStatus])
 
   if(stage == 'checkout')
     return <MarketplaceCheckout />
