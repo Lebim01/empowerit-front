7715a0405a49595ffc24230800c0dc2308b5624f
victor alvarez <victoralvarezsaucedo@gmail.com>
2024-05-21 14:38:11 -0500

    redirect

--- src/views/Views.tsx
+++ src/views/Views.tsx
@@ -44,7 +44,13 @@ const AllRoutes = (props: AllRoutesProps) => {
 
   useEffect(() => {
     if (!isAdmin) {
-      if (!expires || dayjs().isAfter(dayjs(expires))) {
+      if (
+        ['100-pack', '300-pack', '500-pack', '1000-pack', '2000-pack'].includes(
+          user?.membership || ''
+        )
+      ) {
+        setRedirectToPay(false)
+      } else if (!expires || dayjs().isAfter(dayjs(expires))) {
         // ya vencio la membresia
         setRedirectToPay(true)
       } else {
