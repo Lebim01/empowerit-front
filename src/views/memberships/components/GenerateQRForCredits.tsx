d3df8c8ecc5396a2becb57923bf1f641bb7c9589
Skared123 <raymundosanchez62@gmail.com>
2024-07-06 14:32:24 -0700

    creditos con fiat

--- src/views/memberships/components/GenerateQRForCredits.tsx
+++ src/views/memberships/components/GenerateQRForCredits.tsx
@@ -38,6 +38,22 @@ const GenerateQRForCredits = ({
     return (
       <div className="flex flex-col space-y-2">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4">
+          {import.meta.env.VITE_ENABLE_OPENPAY && (
+            <Button
+              className="h-max"
+              disabled={disabled}
+              onClick={() => _create('MXN')}
+            >
+              <div className="flex flex-col items-center space-y-4">
+                <SiCashapp
+                  height={50}
+                  width={50}
+                  className="h-[50px] w-[50px]"
+                />
+                <span>Fiat (MXN)</span>
+              </div>
+            </Button>
+          )}
           <Button
             className="h-max"
             disabled={disabled}
